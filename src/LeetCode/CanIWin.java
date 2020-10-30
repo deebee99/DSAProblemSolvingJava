c6f7ea9870edda33c892aaebcd3113689c4c5a58
deebee99 <40033919+deebee99@users.noreply.github.com>
2020-10-30 21:57:51 +0530

    Corrected CanIWin problem

--- src/LeetCode/CanIWin.java
+++ src/LeetCode/CanIWin.java
@@ -1,41 +1,36 @@
 package LeetCode;//wrong solution
-import java.util.TreeMap;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class CanIWin {
-    TreeMap<Integer,Integer> treeMap=new TreeMap<>();
-
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
-    if(desiredTotal==0) return true;
-    int total=(maxChoosableInteger*(maxChoosableInteger+1))/2;
-    if(total<desiredTotal) return false;
-    if(total==desiredTotal) return maxChoosableInteger%2==0;
-    for(int i=1;i<=maxChoosableInteger;i++) treeMap.put(i,1);
-    return playGame(0,desiredTotal,0);
+        if (desiredTotal <= 0) return true;
+        if (maxChoosableInteger * (maxChoosableInteger + 1) / 2 < desiredTotal) return false;
+        return canIWin(desiredTotal, new int[maxChoosableInteger], new HashMap<>());
     }
 
-    private boolean playGame(int curSum, int total,int playerNo) {
-//        System.out.println(curSum+ " "+playerNo);
-        if(curSum>=total ){
-            if(playerNo==0) return true;
-            return false;
+    private boolean canIWin(int total, int[] state, HashMap<String, Boolean> hashMap) {
+        String curr = Arrays.toString(state);
+        if (hashMap.containsKey(curr)) return hashMap.get(curr);
+        for (int i = 0; i < state.length; i++) {
+            if (state[i] == 0) {
+                state[i] = 1;
+                if (total <= i + 1 || !canIWin(total - (i + 1), state, hashMap)) {
+                    hashMap.put(curr, true);
+                    state[i] = 0;
+                    return true;
+                }
+                state[i] = 0;
+            }
         }
-        boolean ans=false;
-        if(treeMap.containsKey(total-curSum) && treeMap.get(total-curSum)==1){
-            if(playerNo==0) return true;
-            return false;
-        }
-        if(treeMap.ceilingKey(total-curSum)!=null && treeMap.get(treeMap.ceilingKey(total-curSum))==1){
-            if(playerNo==0) return true;
-            return false;
-        }
-        int value=treeMap.lastKey();
-        treeMap.remove(value);
-            return playGame(curSum+value,total,playerNo==0 ? 1 : 0);
+        hashMap.put(curr, false);
+        return false;
     }
 
     public static void main(String[] args) {
-        CanIWin game=new CanIWin();
-        System.out.println(game.canIWin(7,10));
+        CanIWin game = new CanIWin();
+        System.out.println(game.canIWin(7, 10));
 
     }
 }
